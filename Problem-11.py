d5ffd2e49d805099bc8b4c174a8a9d78b05e5f1c
Derya <104157454+sagglamderya@users.noreply.github.com>
2022-06-14 01:12:51 +0300

    Create Problem-11.py

---
+++ Problem-11.py
@@ -0,0 +1,26 @@
+import numpy as np
+f = open("Problem-11txt.txt", "r")
+x=(f.read()).split()
+arr=(np.array(x, dtype=int)).reshape(20,20)
+k=1
+for i in range(19):
+    for j in range(19):
+        try: 
+            
+            hor=(arr[i,j]*arr[i,j+1]*arr[i,j+2]*arr[i,j+3])
+            if hor>k:
+                k=hor
+            prep=(arr[i,j]*arr[i+1,j]*arr[i+2,j]*arr[i+3,j])
+            if prep>k:
+                k=prep
+            cross1=(arr[i,j]*arr[i+1,j+1]*arr[i+2,j+2]*arr[i+3,j+3])
+            if cross1>k:
+                k=cross1
+            cross2=(arr[i,j]*arr[i+1,j-1]*arr[i+2,j-2]*arr[i+3,j-3])
+            if cross2>k:
+                k=cross2              
+        except IndexError:
+            pass
+print(k)
+
+
