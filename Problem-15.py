2cf07c1c5b40b0ec33a9421b6dfe686eb98f6e3d
Derya <104157454+sagglamderya@users.noreply.github.com>
2022-06-23 13:33:07 +0300

    Lattice paths

---
+++ Problem-15.py
@@ -0,0 +1,12 @@
+#Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+#How many such routes are there through a 20×20 grid?
+
+import math
+
+def factorial(x):
+    return math.factorial(x)
+
+def LatticePaths(n,m):
+    return (factorial(n+m)/factorial(n)/factorial(m))
+
+print(int(LatticePaths(20,20)))
