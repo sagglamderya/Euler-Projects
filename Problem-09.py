9f6737c2680e0d8d1ec6f7b36562613d16b13856
Derya <104157454+sagglamderya@users.noreply.github.com>
2022-06-28 04:43:15 +0300

    Special Pythagorean triplet

--- Problem-09.py
+++ Problem-09.py
@@ -2,8 +2,7 @@
 #a^2 + b^2 = c^2
 #For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-#Find the product abc.
-
+#Find the product abc
 def Pythagorean(sum):
     for i in range(1,sum):
         for j in range(1,sum):
